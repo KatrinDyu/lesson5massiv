c0c1e79e49ab07bcd46c0ec9f595fa53fcda031b
Ekaterina <dyuldina32@gmail.com>
2023-01-22 19:35:11 +0500

    3ex

---
+++ ex3/Program.cs
@@ -0,0 +1,35 @@
+﻿/* 38: Задайте массив вещественных чисел. Найдите
+ разницу между максимальным и минимальным элементов массива. */
+
+Console.Clear();
+int[] CreateRandomArray(int N, int start, int end)
+ {
+ int[] RandomArray = new int[N];
+ for (int i = 0; i < N; i++)
+ {
+ RandomArray[i] = new Random().Next(start, end + 1);
+ }
+ return RandomArray;
+ }
+void ShowArray(int[] array)
+{
+ for (int i = 0; i < array.Length; i++)
+ {
+ Console.Write(array[i] + " ");
+ }
+ Console.WriteLine();
+ }
+int[] mass = CreateRandomArray(10, 0, 10);
+ShowArray(mass);
+int diffmaxmin(int[] array)
+{
+    int max = array[0];
+    int min = array[0];
+    foreach(int num in array)
+    {
+        if (num > max) max = num;
+        if (num < min) min = num;
+    }
+return max - min;
+}
+Console.WriteLine(diffmaxmin(mass));
