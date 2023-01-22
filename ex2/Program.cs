907ef1f7b1124900dcc832a7ac7e467f5f0afe36
Ekaterina <dyuldina32@gmail.com>
2023-01-22 19:12:38 +0500

    2 ex

---
+++ ex2/Program.cs
@@ -0,0 +1,34 @@
+﻿/*36: Задайте одномерн ый массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях. */
+
+int[] CreateRandomArray(int N, int start, int end)
+ {
+ int[] RandomArray = new int[N];
+ for (int i = 0; i < N; i++)
+ {
+ RandomArray[i] = new Random().Next(start, end + 1);
+ }
+ return RandomArray;
+ }
+void ShowArray(int[] array)
+{
+ for (int i = 0; i < array.Length; i++)
+ {
+ Console.Write(array[i] + " ");
+ }
+ Console.WriteLine();
+ }
+int[] mass = CreateRandomArray(10, 0, 10);
+ShowArray(mass);
+
+int summeven(int[] array)
+{
+    //int k = 0;
+    int count = 0;
+    for(int k = 1; k < array.Length; k = k + 2)
+    {
+        count = count + array[k];
+    }
+    return count;
+}
+Console.WriteLine(summeven(mass)); 
